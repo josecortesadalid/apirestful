033f9acb4ba36423f61fb99beee8ee11bfea69af
jose <jose.cortes@adalid.net>
2022-09-12 16:23:36 +0200

    Practico desde la parte de Retornando los Errores de Validación en Formato JSON hasta la parte de Controlando Excepciones de No Encontrado (HTTP). Soluciono un error que me impedía obtener las respuestas esperadas, se debía al parámetro request del método render.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,19 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -38,4 +46,34 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    public function render($request, Throwable $exception) // antes puse Throwable
+    {
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if($exception instanceof ModelNotFoundException){
+            $modelo = strtolower(class_basename($exception->getModel())); // solo el nombre de la clase y en minúscula
+            return $this->errorResponse("No existen instancias de {$modelo} con el id que has especificado", 404);
+        }
+        if($exception instanceof AuthenticationException){
+            return $this->unauthenticated($request, $exception);
+        }
+        if($exception instanceof AuthorizationException){
+            return $this->errorResponse('No tiene permisos para ejecutar esta acción', 403);
+        }
+        if($exception instanceof NotFoundHttpException){
+            return $this->errorResponse('No se encontró la URL especificada', 404);
+        }
+        return parent::render($request, $exception);
+    }
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+            return $this->errorResponse('No autenticado.', 401);
+    }
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+        return $this->errorResponse($errors, 422);
+    }
+
 }
